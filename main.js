d569ca85a0185d8a5d4806ac25dfc4170a4c2039
hudsonemiliano-lang <hudson.emiliano@escola.pr.gov.br>
2025-10-29 07:58:56 -0300

    camargo vaz

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-alert("teste")
+const avanca = document.querySelectorAll('.btn-proximo
+    console.log(avanca)
+
+    avanca.forEach(button =>}
+        button.addEventListener('click', function(){
+              const atual =Document.querySelector('.ativoadd')
+              const proximoPasso = 'passo-' + getAttribute('data-')
+
+              atual.classList.remove('ativo')
+              document.getElementById
